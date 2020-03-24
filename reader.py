2bccf692c83130e757e829a8403e1b0d6cb683b4
Tine Makovecki <tine.makovecki@gmail.com>
2020-03-24 18:03:37 +0100

    corrections, added ouput

--- reader.py
+++ reader.py
@@ -1,10 +1,10 @@
-from BooleanFormula import *
+from KnoFormula import *
 from DPLL_simple import DPLL
 
 input_file = "sudoku_mini.txt"
-output_file = "test.txt"
+output_file = "output_test.txt"
 
-
+# read input file
 with open(input_file, mode='r') as input_data:
     lines = input_data.readlines()
     clause_list = []
@@ -16,22 +16,27 @@ with open(input_file, mode='r') as input_data:
         if elements[0] != "c":
             if elements[0] == "p":
                 # save numbers just in case
-                n_vars = elements[2]
-                n_clauses = elements[3]
+                n_vars = int(elements[2])
+                n_clauses = int(elements[3])
             else:
                 # write and save the clause
                 clause = []
-                for el in elements[:-2]:  # the last two elements are "0", "\n"
-                    if "\n" not in elements:
-                        print(elements)
-                    # check if negated
-                    if el[0] == "-":
-                        clause.append(NOT(VARIABLE(el[1:])))
-                    else:
-                        clause.append(VARIABLE(el))
-                clause_list.append(OR(clause))
+                # the last two elements are "0", "\n" so we skip them
+                for el in elements[:-2]:
+                    clause.append(int(el))
+                clause_list.append(clause)
 
-    cnf_formula = AND(clause_list)
+    cnf_formula = KnoFormula(clause_list)
 
+vars = [i for i in range(1, n_vars+1)]
+# vars = tuple(vars)
 
-#solution = DPLL(cnf_formula)
+solution = DPLL(cnf_formula, vars)
+
+# write results in output file
+with open(output_file, mode="w") as out:
+    for i in range(n_vars):
+        if solution[i][1] is True:
+            out.write(str(solution[i][0]) + " ")
+        else:
+            out.write("-" + str(solution[i][0]) + " ")
